1cb7f24166c4200eed564a94094ab88db6a6ebd0
Vibha Masti <vsmasti@gmail.com>
2020-12-02 10:47:17 +0000

    :art: Added theme

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {ThemeProvider} from '@material-ui/core/styles';
 
-import theme from './theme';
+import theme from './theme'; 
 
 import App from './App';
 
