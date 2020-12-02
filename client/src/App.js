1646786170bb32655437c0c5e4e23be36d2a0bcd
Vibha Masti <vsmasti@gmail.com>
2020-12-02 17:41:17 +0000

    :sparkles: Display products from db

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import {
   Typography,
   Container,
   Grid,
+  CircularProgress,
 } from '@material-ui/core';
 
 import {useDispatch, useSelector} from 'react-redux';
@@ -20,6 +21,8 @@ function App() {
   const classes = useStyles();
   const dispatch = useDispatch(); 
   const products = useSelector((state) => state.products);
+
+
   console.log(products);
 
   useEffect(() => {
@@ -27,84 +30,44 @@ function App() {
   }, [dispatch]);
 
   return (
-    <div className="App">
-      <NavBar />
-      
-      <Box className={classes.hero}>
-        <Box>
-          <Typography variant="h2">
-            One stop for all your needs!
-          </Typography>
-        </Box>
-      </Box>
-
-      <Container maxWidth='lg' className={classes.itemsContainer}>
-        <Typography variant="h4" className={classes.itemTitle}>
-          Items
-          </Typography>
+    ! products.length ? <CircularProgress /> : (
+      <div className="App">
+        <NavBar />
         
-        <Grid container spacing={3}>
-          <GridItem 
-            img="https://picsum.photos/200/300" 
-            title="Item 1"
-            avatar='https://i.pinimg.com/originals/5b/c6/e6/5bc6e6b23f963cb859ac7aa748029a78.png'
-            price='₹100'
-            desc='Description for Item 1'
-          />
+        <Box className={classes.hero}>
+          <Box>
+            <Typography variant="h2">
+              One stop for all your needs!
+            </Typography>
+          </Box>
+        </Box>
 
-          <GridItem 
-            img="https://picsum.photos/200/300" 
-            title="Item 2"
-            avatar='https://i.pinimg.com/originals/5b/c6/e6/5bc6e6b23f963cb859ac7aa748029a78.png'
-            price='₹100'
-            desc='Description for Item 2'
-          />
-
-          <GridItem 
-            img="https://picsum.photos/200/300" 
-            title="Item 3"
-            avatar='https://i.pinimg.com/originals/5b/c6/e6/5bc6e6b23f963cb859ac7aa748029a78.png'
-            price='₹100'
-            desc='Description for Item 3'
-          />
-
-          <GridItem 
-            img="https://picsum.photos/200/300" 
-            title="Item 4"
-            avatar='https://i.pinimg.com/originals/5b/c6/e6/5bc6e6b23f963cb859ac7aa748029a78.png'
-            price='₹100'
-            desc='Description for Item 4'
-          />
-
-          <GridItem 
-            img="https://picsum.photos/200/300" 
-            title="Item 5"
-            avatar='https://i.pinimg.com/originals/5b/c6/e6/5bc6e6b23f963cb859ac7aa748029a78.png'
-            price='₹100'
-            desc='Description for Item 5'
-          />
+        <Container maxWidth='lg' className={classes.itemsContainer}>
+          <Typography variant="h4" className={classes.itemTitle}>
+            Items
+            </Typography>
           
-          <GridItem 
-            img="https://picsum.photos/200/300" 
-            title="Item 6"
-            avatar='https://i.pinimg.com/originals/5b/c6/e6/5bc6e6b23f963cb859ac7aa748029a78.png'
-            price='₹100'
-            desc='Description for Item 6'
-          />
+          <Grid container spacing={3}> {
+            
+            products.map((product) => (
+  
+              <GridItem key={product._id}
+                img={!product.image? "https://picsum.photos/200/300": product.image}
+                title={product.name}
+                avatar='https://i.pinimg.com/originals/5b/c6/e6/5bc6e6b23f963cb859ac7aa748029a78.png'
+                price={'₹' + product.price}
+                desc={product.description}
+              />
+            ))
+          }
 
-          <GridItem 
-            img="https://picsum.photos/200/300" 
-            title="Item 7"
-            avatar='https://i.pinimg.com/originals/5b/c6/e6/5bc6e6b23f963cb859ac7aa748029a78.png'
-            price='₹100'
-            desc='Description for Item 7'
-          />
+          </Grid>
+        </Container>
 
-        </Grid>
-      </Container>
-
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    )
+    
   );
 }
 
