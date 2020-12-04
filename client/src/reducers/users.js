d227771ce4104bb2571c9ec113ffe8fe97822fe7
Vishruth <vishruth2002@gmail.com>
2020-12-04 10:41:34 +0530

    Linked up SignIn with login api to validate users

--- client/src/reducers/users.js
+++ client/src/reducers/users.js
@@ -2,6 +2,9 @@ const reducer = (state = [], action) => {
     switch (action.type) {
       case 'CREATE_USER':
         return [...state, action.payload];
+      case 'USER_LOGIN':
+          //return [...state, action.payload];
+          return action.payload;
       default:
         return state;
     }
