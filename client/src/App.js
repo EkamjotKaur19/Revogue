fd671ccc5ebf40e1e43fc27eb33fe5f6ed73298b
Vibha Masti <39802747+vibhamasti@users.noreply.github.com>
2020-12-06 17:58:11 +0530

    Revert "Added Cart folder in components to create shopping cart"

--- client/src/App.js
+++ client/src/App.js
@@ -19,29 +19,13 @@ import Products from './components/Products/Products';
 import ProductsByCat from './components/Products/ProductsByCat';
 import ScrollToTop from './ScrollToTop';
 
-import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
-
-// added by Vedant
-import React from 'react';
-import { useGlobalContext } from './components/Cart/context';
-import CartContainer from './components/Cart/CartContainer'; 
+import {BrowserRouter as Router, Route, withRouter} from 'react-router-dom';
 
 
 function App() {
-  // const classes = useStyles();
+  const classes = useStyles();
   
   sessionStorage.setItem('cart', '[]');
-
-  const { state: { loading }, } = useGlobalContext();
-
-  if (loading)
-  {
-    return(
-      <div className='loading'>
-      <h1>Loading...</h1>
-      </div>
-    );
-  }
 
   return (
     <div className="App">
@@ -50,25 +34,24 @@ function App() {
         <ScrollToTop>
           <Route exact path = "/login" component = {SignIn} />
           <Route exact path = "/checkout" component = {Checkout} />
-          <Route exact path="/signup" component={SignUp} />
-          {/* added */}
-          <Route exact path = "/cart" component = {CartContainer} />  
+          <Route exact path = "/signup" component = {SignUp} />
           <Route exact path='/' component={Products} />
           <Route path = "/products/:id" render={(props) => <ProductItem {...props}/>} />
           <Route path = "/products/cat/:cat" render={(props) => <ProductsByCat {...props} />}/>
           <Route exact path = "/cart" component = {Cart} />
         </ScrollToTop>
+        
         {/* <Route path = "/products/:id" component = {ProductItem} /> */}
-      </Router>     
+      
+      
+      </Router>
+      
+      
+      
+
       {/* <Products /> */}
+
       <Footer />
-
-      {/* Added from here */}
-
-      {/* Added by vedant */}
-      <CartContainer />
-
-      );
 
     </div>
     
